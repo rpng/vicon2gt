d5583f9f4442e41016d103092d00f357b46282d9
cmb <cmburgul@gmail.com>
2024-09-14 10:30:58 -0400

    Ghost IMU noises

--- scripts/run_quad_groundtruth.sh
+++ scripts/run_quad_groundtruth.sh
@@ -1,33 +1,52 @@
 #!/usr/bin/env bash
 
-source /home/patrick/workspace/catkin_ws_ov/devel/setup.bash
+source /home/cmb/singularity/workspaces_ros1/vicon_ws/devel/setup.bash
 
 
-DATASET="/media/patrick/RPNG FLASH 3/quad/raw_data/"
+DATASET="/media/cmb/T7/singularity_data/dataset/ghost/sysID/data_sept10/raw_data"
 BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-
-
 bagnames=(
-    "mav_imu_joints_GRF/mavSystemID_1"
-    "mav_imu_joints_GRF/mavSystemID_2"
-    "mav_imu_joints_GRF/mavSystemID_3"
-    "mav_imu_joints_GRF/mavSystemID_4"
+    "imu/sample"
 )
+
 bagnames_gt=(
-    "vicon_data/session1"
-    "vicon_data/session2"
-    "vicon_data/session3"
-    "vicon_data/session4"
+    "vicon_data/sample"
 )
 
 bagdurr=(
     "-1"
-    "-1"
-    "-1"
-    "-1"
 )
 
+#bagnames=(
+#    "imu/sample"
+#    "imu/sample2"
+#    "imu/session1"
+#    "imu/session2"
+#    "imu/session3"
+#    "imu/session4"
+#    "imu/session5"
+#)
+#
+#bagnames_gt=(
+#    "vicon_data/sample"
+#    "vicon_data/sample2"
+#    "vicon_data/session1"
+#    "vicon_data/session2"
+#    "vicon_data/session3"
+#    "vicon_data/session4"
+#    "vicon_data/session5"
+#)
+#
+#bagdurr=(
+#    "-1"
+#    "-1"
+#    "-1"
+#    "-1"
+#    "-1"
+#    "-1"
+#    "-1"
+#)
 
 rm -rf "$DATASET/groundtruth/"
 rm -rf "$DATASET/groundtruth_info/"
@@ -38,21 +57,19 @@ mkdir -p "$DATASET/groundtruth_info/"
 # Loop through all datasets
 for i in "${!bagnames[@]}"; do
 
+echo "path_bag:=$DATASET/${bagnames[i]}.bag"
+echo "path_bag_gt:=$DATASET/${bagnames_gt[i]}.bag"
+
 roslaunch "$BASEDIR/../launch/exp_quad.launch" \
     path_bag:="$DATASET/${bagnames[i]}.bag" \
     path_bag_gt:="$DATASET/${bagnames_gt[i]}.bag" \
     bag_durr:="${bagdurr[i]}" \
     stats_path_states:="$DATASET/groundtruth/${bagnames[i]}.csv" \
     stats_path_info:="$DATASET/groundtruth_info/${bagnames[i]}.txt" \
-    topic_imu:="/mav_imu" \
+    topic_imu:="/mcu/state/d_imu" \
     topic_vicon:="/ghostgt/world"
 
 done
 
-
 # convert to the right format
-rosrun ov_eval format_converter "$DATASET/groundtruth/" &> /dev/null
-
-
-
-
+#rosrun ov_eval format_converter "$DATASET/groundtruth/" #&> /dev/null
